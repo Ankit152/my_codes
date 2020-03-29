f8414ebf4930193a0e1b5583b5d9525d75ce2d55
Ankit Kurmi <ankitkurmi152@gmail.com>
2020-03-29 12:48:34 +0530

    automorphic.java

---
+++ codes/java codes/Automorphic.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class automorphic{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n;
+        boolean flag=true;
+        System.out.println("Enter a number: ");
+        n=sc.nextInt();
+        int k=n*n;
+        while(n>0){
+            if((n%10)!=(k%10)){
+                flag=false;
+                break;
+            }
+            n/=10;
+            k/=10;
+        }
+        if(flag) System.out.println("Automorphic Number !!!");
+        else System.out.println("Not an Automorphic NUmber !!!");
+    }
+}

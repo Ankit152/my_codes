48fbcab18f4d87f736cd87563cfcaebeff6e5f64
Ankit Kurmi <ankitkurmi152@gmail.com>
2020-03-31 12:34:47 +0530

    implemented basic java string input

---
+++ codes/java codes/stringInput.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class stringInput{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(s);
+    }
+}

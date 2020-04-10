a8ac436bf0358669f5d8590b81d0282df016b10b
Ankit152 <ankitkurmi152@gmail.com>
2020-04-10 13:26:30 +0530

    added a java code for maximum among 3

---
+++ codes/java_codes/maxof3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class maxof3{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number 1: ");
+        int a = sc.nextInt();
+        System.out.println("Enter a number 2: ");
+        int b = sc.nextInt();
+        System.out.println("Enter a number 3: ");
+        int c = sc.nextInt();
+        int m=a;
+        if(b>a && b>c){
+            m=b;
+        }
+        else if(c>a && c>b){
+            m=c;
+        }
+        System.out.println("Maxium of the three is: "+m);
+    }
+}

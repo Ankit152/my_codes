260729088add67d809b0b82105393f955853cf40
Ankit152 <ankitkurmi152@gmail.com>
2020-04-22 22:30:24 +0530

    made changes

--- codes/java_codes/arraydec.java
+++ codes/java_codes/arraydec.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class arraydec {
+class arraydec {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array: ");
@@ -13,5 +13,6 @@ public class arraydec {
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
+        sc.close();
     }
 }

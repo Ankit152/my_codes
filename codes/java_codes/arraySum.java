ab21c516b91d1c513edcde7cb4bc0a3adfda55f2
Ankit152 <ankitkurmi152@gmail.com>
2020-04-22 22:31:15 +0530

    added changes

--- codes/java_codes/arraySum.java
+++ codes/java_codes/arraySum.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class arraySum {
+class arraySum {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n;
@@ -11,6 +11,7 @@ public class arraySum {
         }
         int sum = SUM(arr);
         System.out.println("THe array sum is: "+sum);
+        sc.close();
     }
     public static int SUM(int[] arr){
         int sum=0;

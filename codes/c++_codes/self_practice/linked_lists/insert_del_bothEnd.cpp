6809aa641c774131ff874717c4f9ea1fe2121678
Ankit152 <ankitkurmi152@gmail.com>
2020-05-09 22:14:18 +0530

    linked list class

---
+++ codes/c++_codes/self_practice/linked_lists/insert_del_bothEnd.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+        int data;
+        node *next;
+        node(int data){
+            this->data=data;
+            next=NULL;
+        }
+};
